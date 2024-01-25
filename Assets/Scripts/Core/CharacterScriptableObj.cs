653093a8f41a853288e16d902f27d4416b96e813
Mahmud Mizan Fahim <76226888+fahimtanvir003@users.noreply.github.com>
2024-01-25 13:48:56 +0600

    Basic setup
    Folder creation, core scripts and player prefab

---
+++ Assets/Scripts/Core/CharacterScriptableObj.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterScriptableObj", menuName = "ScriptableObjects/Character")]
+public class CharacterScriptableObj : ScriptableObject
+{
+    public GameObject character;
+    public int health;
+}
